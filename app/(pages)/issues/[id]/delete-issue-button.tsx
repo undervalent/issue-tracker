24be7ec167f11c0762dc6fc9c708001155941878
Jon Ochoa <undervalent@gmail.com>
2023-11-10 15:52:50 -0800

    feat(delete issue): handled deleting issue

--- app/(pages)/issues/[id]/delete-issue-button.tsx
+++ app/(pages)/issues/[id]/delete-issue-button.tsx
@@ -2,9 +2,11 @@
 import React from 'react'
 import { TrashIcon } from '@radix-ui/react-icons';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 export default function DeleteIssueButton({ issueId }: { issueId: number }) {
-
+  const router = useRouter();
 
   return (
     <AlertDialog.Root>
@@ -23,7 +25,11 @@ export default function DeleteIssueButton({ issueId }: { issueId: number }) {
           </AlertDialog.Cancel>
           <AlertDialog.Action>
 
-            <Button color='red' variant='soft'>Delete Issue</Button>
+            <Button color='red' variant='soft' onClick={async () => {
+              axios.delete(`/api/issues/${issueId}`);
+              router.push('/issues');
+              router.refresh();
+            }}>Delete Issue</Button>
           </AlertDialog.Action>
         </Flex>
 
