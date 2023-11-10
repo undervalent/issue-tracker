36d2200107ebb6c00088080d53cd6e61aefda8e4
Jon Ochoa <undervalent@gmail.com>
2023-11-10 15:46:58 -0800

    feat(delete api): built api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -29,3 +29,21 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
   return NextResponse.json(updatedIssue)
 
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+  const paramId = parseInt(params.id)
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: paramId }
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 })
+  }
+
+  await prisma.issue.delete({
+    where: { id: paramId }
+  })
+
+  return NextResponse.json({})
+}
