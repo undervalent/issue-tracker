510f5f2a83781424efa6819b9baa748e6d396920
Jon Ochoa <undervalent@gmail.com>
2023-11-10 15:40:44 -0800

    feat(confirmation): show confirmation dialogue

--- app/(pages)/issues/[id]/delete-issue-button.tsx
+++ app/(pages)/issues/[id]/delete-issue-button.tsx
@@ -1,10 +1,33 @@
-
+"use client"
 import React from 'react'
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 
 export default function DeleteIssueButton({ issueId }: { issueId: number }) {
+
+
   return (
-    <Button color='red'> <TrashIcon /> Delete Issue</Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'> <TrashIcon /> Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue?
+          This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button color='gray' variant='soft'>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+
+            <Button color='red' variant='soft'>Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
