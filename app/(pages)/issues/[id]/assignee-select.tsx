f1574fd6881d1113123f7a1793ecf9d40ed3541c
Jon Ochoa <undervalent@gmail.com>
2023-11-13 21:07:12 -0800

    feat(assignee select): populate selection

--- app/(pages)/issues/[id]/assignee-select.tsx
+++ app/(pages)/issues/[id]/assignee-select.tsx
@@ -1,8 +1,20 @@
 "use client"
-import { Select } from '@radix-ui/themes'
 import React from 'react'
+import { Select } from '@radix-ui/themes'
+import { User } from '@prisma/client';
+import axios from 'axios';
 
 export default function AssigneeSelect() {
+  const [users, setUsers] = React.useState<User[]>([]);
+
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>('/api/users');
+      setUsers(data)
+
+    }
+    fetchUsers();
+  }, [])
   return (
     <Select.Root>
       <Select.Trigger placeholder='Assign to...' />
@@ -11,9 +23,13 @@ export default function AssigneeSelect() {
           <Select.Label>
             Suggestions
           </Select.Label>
-          <Select.Item value="1">
-            Jon Ochoa
-          </Select.Item>
+          {users.map((user) => {
+            return (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            )
+          })}
         </Select.Group>
       </Select.Content>
     </Select.Root>
