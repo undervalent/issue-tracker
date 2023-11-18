2da2c190be872c5ced8bbf29c62c5dc25de10b18
Jon Ochoa <undervalent@gmail.com>
2023-11-17 20:15:11 -0800

    feat(pagination): implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Pagination from "@/lib/components/pagination";
 
-export default function Home() {
+export default function Home({ searchParams }: {
+  searchParams: {
+    page: string
+  }
+}) {
+
   return (
     <section>
-      <Pagination itemCount={100} currentPage={1} pageSize={10} />
+      <Pagination itemCount={100} currentPage={parseInt(searchParams.page)} pageSize={10} />
     </section>
   )
 }
