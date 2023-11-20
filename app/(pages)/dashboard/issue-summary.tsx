3d1c566a9f20b84374adf31000395b3f3b6f42fa
Jon Ochoa <undervalent@gmail.com>
2023-11-20 13:41:40 -0800

    feat(summary): build summary component

---
+++ app/(pages)/dashboard/issue-summary.tsx
@@ -0,0 +1,47 @@
+import { Status } from '@prisma/client';
+import { Card, Flex, Text } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react'
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+export default function IssueSummary({ open, inProgress, closed }: Props) {
+  const containers: {
+    label: string;
+    value: number;
+    status: Status
+  }[] = [
+      {
+        label: 'Open Issues',
+        value: open,
+        status: "OPEN"
+      },
+      {
+        label: 'Closed Issues',
+        value: closed,
+        status: "CLOSED"
+      },
+      {
+        label: 'In Progress Issues',
+        value: inProgress,
+        status: "IN_PROGRESS"
+      }
+    ]
+  return (
+    <Flex gap="4">
+      {containers.map((container) => <Card key={container.label}>
+        <Flex direction="column" gap="1">
+          <Link href={`/issues/?status=${container.status}`}
+            className='text-sm font-medium'
+          >{container.label}</Link>
+          <Text size="5" className='font-bold'>{container.value}</Text>
+        </Flex>
+      </Card>)}
+
+    </Flex>
+  )
+}
--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,8 +1,23 @@
 import LatestIssues from "./latest-issues"
+import IssueSummary from "./issue-summary"
+import prisma from "@/prisma/client"
 
+async function useIssueCounts() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } })
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } })
+  const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } })
 
-export default function Dashboard() {
+  return [{
+    open,
+    closed,
+    inProgress
+  }]
+}
+
+export default async function Dashboard() {
+  const [{ open, closed, inProgress }] = await useIssueCounts();
   return <section>
     <LatestIssues />
+    <IssueSummary open={open} closed={closed} inProgress={inProgress} />
   </section>
 }
